cb90d1f7eb08157eb51f8000202fe78a616325ee
kulvinder kakar <kulvinderkakar14@gmail.com>
2021-12-10 08:42:21 +1100

    logs

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const jsreport = require('./')({ rootDirectory: __dirname })
-
+jsreport.use(require('jsreport-phantom-image')({ strategy: 'phantom-server', proxy: 'http://tdcproxycorp.actewagl.com.au:8080'}))
 if (process.env.JSREPORT_CLI) {
   module.exports = jsreport
 } else {
   jsreport.init().then(() => {
+    console.log("JSREPORT INITIALISED")
   }).catch((e) => {
     console.log("Error:",e)
     console.trace(e)
