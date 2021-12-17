0fcece21bf926ee491999e841ef52623b2d87a04
kulvinder kakar <kulvinderkakar14@gmail.com>
2021-12-17 13:39:10 +1100

    added server logs

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ if (process.env.JSREPORT_CLI) {
 } else {
   jsreport.init().then(() => {
   }).catch((e) => {
-    console.log("Error:",e)
-    console.trace(e)
+    console.log("Error:",e);
+    console.trace(e);
     process.exit(1)
   })
 }
